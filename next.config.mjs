732a270b2dc11a07b80fe55aed65467746edbae6
Muntasir Mallik <73852736+muntaxir4@users.noreply.github.com>
2025-07-06 21:01:09 +0530

    Reset Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["images.unsplash.com", "cdn.pixabay.com"],
-  },
+  experimental: {
+    esmExternals: 'loose'
+  }
 };
 
 export default nextConfig;
